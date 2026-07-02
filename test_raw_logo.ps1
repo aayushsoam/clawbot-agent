76cd419a0fa37fa33550f9c1f8057f5b8e609b06
Aayush Soam <145256594+aayushsoam@users.noreply.github.com>
2026-07-03 01:36:47 +0530

    7/3/2026

---
+++ test_raw_logo.ps1
@@ -0,0 +1,6 @@
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+Write-Host "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄" -ForegroundColor Red
+Write-Host "██░▄▄▄████░███████░▄▄▄░████░███░███░████░▄▄▄░████░▄▄▄░███▄▄▄░▄▄▄██" -ForegroundColor Red
+Write-Host "██░███████░███████░▀▀▀░████░███░███░████░▀▀▀░████░███░██████░█████" -ForegroundColor Red
+Write-Host "██░▀▀▀████░▀▀▀▀███░███░████░▀▀▀░▀▀▀░████░▀▀▀░████░▀▀▀░██████░█████" -ForegroundColor Red
+Write-Host "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" -ForegroundColor Red
